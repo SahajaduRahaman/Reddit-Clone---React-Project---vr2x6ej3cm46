de604db77035f5f7d7130d1ddfd4513e8a21c7c4
Raza <sahajadurahaman@gmail.com>
2023-02-02 20:40:48 +0530

    jhhj

--- src/pages/Home/Post.js
+++ src/pages/Home/Post.js
@@ -58,7 +58,7 @@ const Post = (props) => {
 
   return (
     <div>
-      <Card sx={{ minWidth: 275, bgcolor: '#1FE29A'}}>
+      <Card sx={{ minWidth: 275, bgcolor: '#4CCB88'}}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
